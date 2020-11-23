237d31b4c0dff3c3831c04f437f781e5a8c65fa5
Tomáš Reinert <reinerttomas@gmail.com>
2020-11-23 22:36:34 +0100

    dirty template

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer v-show="isFooterBarVisible" class="footer">
+    <div class="container-fluid">
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <div class="footer-copyright">
+              <b>&copy; {{ year }}, JustBoil.me</b> &mdash; Admin One Demo
+              <span class="tag">v1.5.0</span>
+            </div>
+          </div>
+        </div>
+        <div class="level-right">
+          <div class="level-item">
+            <div class="logo">
+              <a href="https://justboil.me">
+                <img src="../assets/justboil-logo.svg" alt="JustBoil.me" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "FooterBar",
+  computed: {
+    year() {
+      return new Date().getFullYear();
+    },
+    ...mapState(["isFooterBarVisible"]),
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,77 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <title-bar :title-stack="titleStack" />
+    <hero-bar :has-right-visible="false">
+      Dashboard
+    </hero-bar>
+    <section class="section is-main-section">
+      <tiles>
+        <card-widget
+          class="tile is-child"
+          type="is-primary"
+          icon="account-multiple"
+          :number="512"
+          label="Clients"
+        />
+        <card-widget
+          class="tile is-child"
+          type="is-info"
+          icon="cart-outline"
+          :number="7770"
+          prefix="$"
+          label="Sales"
+        />
+        <card-widget
+          class="tile is-child"
+          type="is-success"
+          icon="chart-timeline-variant"
+          :number="256"
+          suffix="%"
+          label="Performance"
+        />
+      </tiles>
+
+      <card-component title="Clients" class="has-table has-mobile-sort-spaced">
+        <clients-table-sample
+          :data-url="`${$router.options.base}data-sources/clients.json`"
+        />
+      </card-component>
+    </section>
+  </div>
 </template>
+
+<script>
+import TitleBar from "@/components/TitleBar";
+import HeroBar from "@/components/HeroBar";
+import Tiles from "@/components/Tiles";
+import CardWidget from "@/components/CardWidget";
+import CardComponent from "@/components/CardComponent";
+import ClientsTableSample from "@/components/ClientsTableSample";
+
+export default {
+  name: "Home",
+  components: {
+    ClientsTableSample,
+    CardComponent,
+    CardWidget,
+    Tiles,
+    HeroBar,
+    TitleBar,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    titleStack() {
+      return ["Admin", "Dashboard"];
+    },
+  },
+  mounted() {
+    this.$buefy.snackbar.open({
+      message: "Welcome back",
+      queue: false,
+    });
+  },
+  methods: {},
+};
+</script>
